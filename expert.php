55cb928285c277d34daf73af45497f1fd6a5a0bd
Michiel Coulier <michielcoulier@gmail.com>
2020-10-02 10:01:44 +0200

    first 2 errors solved

---
+++ expert.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+
+// Below are several code blocks, read them, understand them and try to find whats wrong.
+// Once this exercise is finished, we'll go over the code all together and we can share how we debugged the following problems.
+// Try to fix the code every time and good luck ! (write down how you found out the answer and how you debugged the problem)
+// =============================================================================================================================
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+// SOLUTION: need to define variable otherwise it won't recognize it
+function new_exercise() {
+    $x = null;
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+
+}
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+// SOLUTION: Week[1] displays tuesday because array starts at 0
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = “Debugged ! Also very fun”;
+echo substr($str, 0, 10);
