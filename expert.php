bae6c79181400f6b1c15e80298df963f3c849ef5
Michiel Coulier <michielcoulier@gmail.com>
2020-10-02 14:35:47 +0200

    exc 8 solved I think, no access looks weird in 1 string

--- expert.php
+++ expert.php
@@ -114,9 +114,8 @@ echo copyright((int)date('Y'));
 
 new_exercise(8);
 function login(string $email, string $password) {
-    if($email == 'john@example.be' || $password == 'pocahontas') {
-        return 'Welcome John';
-        return ' Smith';
+    if($email == 'john@example.be' && $password == 'pocahontas') {
+        return 'Welcome John Smith';
     }
     return 'No access';
 }
